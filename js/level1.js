e0cf2d29377a6e5192a5310497fa67af51e6fa01
Mr Burt <alan.burt@mackillop.act.edu.au>
2020-05-08 10:41:26 +1000

    Game Template files (Mr. Burt)
    Upload of the files from the game template.

---
+++ js/level1.js
@@ -0,0 +1,16 @@
+/**
+ * A single level of the game.
+ * You will need multiple copies of this for each level you
+ * want to include.
+ * Make sure they have different file names and different state names.
+ * level2State etc will work fine
+ */
+var level1State = {
+   create: function() {
+      // create the level
+   },
+
+   update: function() {
+      // do things on the game loop
+   }
+};
---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a game menu
+ * use game.state.start('level1') to go level1State
+ */
+var menuState = {
+   create: function() {
+      // create the scene
+   }
+};
